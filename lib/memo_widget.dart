1e1d6519ab6eb8be90d875db398ff92b02a029ac
TaeBbong <mok05289@korea.ac.kr>
2020-01-05 16:15:52 +0900

    Add description to Draggable

--- lib/memo_widget.dart
+++ lib/memo_widget.dart
@@ -1,3 +1,4 @@
+import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
 
 class MemoWidget extends StatelessWidget {
@@ -32,15 +33,36 @@ class MemoWidget extends StatelessWidget {
       ),
       feedback: Container(
         margin: const EdgeInsets.symmetric(vertical: 10.0),
-        child: Container(
-          margin: const EdgeInsets.only(left: 16.0),
-          child: CircleAvatar(
-            child: Text(text[0],
-                style: TextStyle(
-                  color: Colors.white,
-                )),
-            backgroundColor: c,
-          ),
+        child: Row(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(left: 16.0),
+              child: CircleAvatar(
+                child: Text(text[0],
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+                backgroundColor: c,
+              ),
+            ),
+            Container(
+              width: 250,
+              child: Bubble(
+                nip: BubbleNip.leftTop,
+                child: Text(
+                  text,
+                  style: TextStyle(
+                      fontSize: 11,
+                      color: Colors.black,
+                      decoration: TextDecoration.none),
+                  maxLines: 3,
+                ),
+                margin: BubbleEdges.only(right: 30.0),
+                // elevation: 1 * 0.5,
+                alignment: Alignment.topLeft,
+              ),
+            )
+          ],
         ),
       ),
       childWhenDragging: Container(),
